32d1368f71ccaa3faf917c23b75bace300f4103c
wixner <wixner@3650c4fd-ea0f-e2d6-5ead-89630c913e81>
2011-10-18 10:34:36 +0000

    Install guice modules in PortalModule instead of the listener, moving clbmybatis properties into the single application.properties

--- portal-web/trunk/src/main/java/org/gbif/portal/config/PortalModule.java
+++ portal-web/trunk/src/main/java/org/gbif/portal/config/PortalModule.java
@@ -1,6 +1,9 @@
 package org.gbif.portal.config;
 
 import org.gbif.checklistbank.search.inject.SearchModule;
+import org.gbif.checklistbank.service.mybatis.guice.ChecklistBankServiceMyBatisModule;
+import org.gbif.occurrencestore.ws.client.guice.OccurrenceWsClientModule;
+import org.gbif.registry.ws.client.guice.RegistryWsClientModule;
 
 import java.io.IOException;
 import java.util.Properties;
@@ -11,6 +14,11 @@ import com.google.inject.name.Names;
 public class PortalModule extends AbstractModule {
 
   /**
+   * The portal module combines all service modules needed for the portal.
+   * You can switch between the mybatis and the ws-client api implementation for checklistbank
+   * by swapping the used module below.
+   * When mybatis is used make sure your maven settings include the checklistbank jdbc properties.
+   *
    * @throws ConfigurationException If the application properties cannot be read
    */
   private void bindApplicationProperties() throws ConfigurationException {
@@ -19,7 +27,13 @@ public class PortalModule extends AbstractModule {
       properties.load(this.getClass().getResourceAsStream("/application.properties"));
       Names.bindProperties(binder(), properties);
 
+      // install modules
       install(new SearchModule(properties));
+      install(new RegistryWsClientModule());
+      install(new OccurrenceWsClientModule());
+      // select either the mybatis or the ws-client api implementation:
+      install(new ChecklistBankServiceMyBatisModule(properties));
+      //install(new ChecklistBankWsClientModule());
 
     } catch (IOException e) {
       throw new ConfigurationException(
