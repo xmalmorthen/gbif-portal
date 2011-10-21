d666d938b4bdebf6946829581c82754bc2144eeb
lars.francke@gmail.com <lars.francke@gmail.com@3650c4fd-ea0f-e2d6-5ead-89630c913e81>
2011-10-21 12:08:50 +0000

    Commit simple Selenium Test. Runs in the integration-test phase.

---
+++ portal-web/trunk/src/test/java/org/gbif/portal/SeleniumIT.java
@@ -0,0 +1,37 @@
+package org.gbif.portal;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+public class SeleniumIT {
+
+  private WebDriver driver;
+  private final String baseUrl = "http://localhost:8080/";
+
+  @Before
+  public void setUp() throws Exception {
+    driver = new HtmlUnitDriver();
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void testFoo() throws Exception {
+    driver.get(baseUrl);
+    driver.findElement(By.linkText("Datasets")).click();
+    driver.findElement(By.cssSelector("button.search_button")).click();
+    driver.findElement(By.xpath("//*[contains(.,'CATE Araceae')]"));
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    if (driver != null) {
+      driver.quit();
+    }
+  }
+}
