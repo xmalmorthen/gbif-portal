dc289de26718862ed64bc7582c1e3c337528f406
lars.francke@gmail.com <lars.francke@gmail.com@3650c4fd-ea0f-e2d6-5ead-89630c913e81>
2011-10-27 14:26:37 +0000

    Add a small test for the species search (which fails at the moment).

--- portal-web/trunk/src/test/java/org/gbif/portal/SpeciesSeleniumIT.java
+++ portal-web/trunk/src/test/java/org/gbif/portal/SpeciesSeleniumIT.java
@@ -2,8 +2,10 @@ package org.gbif.portal;
 
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class SpeciesSeleniumIT extends SeleniumTestBase {
 
@@ -15,6 +17,31 @@ public class SpeciesSeleniumIT extends SeleniumTestBase {
     getDriver().get(getBaseUrl() + "/species/1");
     String targetName = getDriver().findElement(By.xpath("//div[@id='infoband']/div/h1")).getText();
     assertEquals("Animalia", targetName);
+  }
+
+  @Test
+  public void testSpeciesSearch() {
+    assertHasSearchResults("albies");
+    assertHasSearchResults("puma");
+    assertHasSearchResults("puma concolor");
+    assertHasNoSearchResults("fooooobaetrniedtruinatedi2lc3c^e/eiu");
+  }
+
+  private void assertHasNoSearchResults(String search) {
+    // TODO: Can't add yet because search doesn't work and I don't know yet what'll be returned on no result
+  }
+
+  /**
+   * Searches for a string on the species search page and asserts that there are some results for this by looking for
+   * the string 'results for "searchstring"'.
+   */
+  private void assertHasSearchResults(String search) {
+    getDriver().get(getBaseUrl() + "/species/");
+    WebElement element = getDriver().findElement(By.name("q"));
+    element.sendKeys(search);
+    getDriver().findElement(By.xpath("/html/body/div/article/div/form/button")).click();
+    String targetName = getDriver().findElement(By.xpath("/html/body/div/form/article/div/div/div/h2")).getText();
+    assertTrue(targetName.contains("results for \"" + search + '\"'));
   }
 
 }
