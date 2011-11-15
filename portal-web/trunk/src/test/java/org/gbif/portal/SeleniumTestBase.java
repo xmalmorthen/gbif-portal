52ffa7e822ae826ad79be73f4a4c0552ded68944
wixner <wixner@3650c4fd-ea0f-e2d6-5ead-89630c913e81>
2011-11-15 13:00:36 +0000

    Adding vernacular names search selenium tests

--- portal-web/trunk/src/test/java/org/gbif/portal/SeleniumTestBase.java
+++ portal-web/trunk/src/test/java/org/gbif/portal/SeleniumTestBase.java
@@ -1,11 +1,15 @@
 package org.gbif.portal;
 
 import java.util.concurrent.TimeUnit;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Base class for Selenium test providing {@code Before} and {@code After} methods that set up a {@link HtmlUnitDriver}
@@ -13,9 +17,11 @@ import org.openqa.selenium.htmlunit.HtmlUnitDriver;
  */
 public abstract class SeleniumTestBase {
 
-  private WebDriver driver;
+  protected final Logger LOG = LoggerFactory.getLogger(getClass());
+  protected WebDriver driver;
   private final String baseUrl;
   private static final long DEFAULT_TIMEOUT = 30;
+  private static final Pattern idLinkPattern = Pattern.compile("/([0-9]+)$");
 
   protected SeleniumTestBase() {
     baseUrl = "http://localhost:" + System.getProperty("jetty.port", "8080");
@@ -34,6 +40,14 @@ public abstract class SeleniumTestBase {
     }
   }
 
+  protected Integer parseIdLink(String link){
+    Matcher m = idLinkPattern.matcher(link);
+    if (m.find()){
+      return Integer.parseInt(m.group(1));
+    }
+    return null;
+  }
+
   protected WebDriver getDriver() {
     return driver;
   }
@@ -41,4 +55,9 @@ public abstract class SeleniumTestBase {
   protected String getBaseUrl() {
     return baseUrl;
   }
+
+  protected String getPortalUrl(String subpath) {
+    return baseUrl + "/" + subpath;
+  }
+
 }
---
+++ portal-web/trunk/src/test/java/org/gbif/portal/SpeciesSearchSeleniumIT.java
@@ -0,0 +1,65 @@
+package org.gbif.portal;
+
+import java.util.LinkedList;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+public class SpeciesSearchSeleniumIT extends SeleniumTestBase {
+
+  @Test
+  public void testIdPattern() {
+    assertEquals((Integer) 2685464, parseIdLink("http://staging.gbif.org:8080/portal-web-dynamic/species/2685464"));
+    assertEquals((Integer) 1, parseIdLink("http://localhost:8080/species/1"));
+    assertNull(parseIdLink("http://localhost:8080/species/search"));
+  }
+
+  /**
+   * This test uses the dataset page to search for all datasets, clicks on the second one and verifies that
+   * the details page for the selected dataset is shown.
+   */
+  @Test
+  @Ignore
+  public void testVernacularNameSearch() {
+    assertNameSearch("Abies pinsapo ", true, 16, 2685464);
+    assertNameSearch("Spanische TANNE", true, 1, 2685464);
+    assertNameSearch("španjolska jela", true, 1, 2685464);
+  }
+
+  /**
+   * @param name the q name to search for
+   * @param searchNub true if only the nub is being searched, false to search all
+   * @param expectedUsageId the list of expected name usage ids in the result, starting with first entry
+   */
+  private void assertNameSearch(String name, boolean searchNub, Integer expectedNumResults, int ... expectedUsageId){
+    LOG.debug("Asserting name search '{}'", name);
+    driver.get(getPortalUrl("species"));
+    // Find input form and enter seach text
+    WebElement input = driver.findElement(By.name("q"));
+    input.sendKeys(name);
+    input.submit();
+    //driver.findElement(By.cssSelector("input.search_button")).click();
+
+    // main content div on page
+    WebElement content = driver.findElement(By.id("content"));
+
+    // assert number of hits
+    if (expectedNumResults != null){
+      assertEquals("Expected number of shown search results wrong", expectedNumResults, (Integer) content.findElements(By.cssSelector("div.result")).size());
+    }
+
+    // assert exact results
+    LinkedList<WebElement> links = new LinkedList(content.findElements(By.cssSelector("div.result a")));
+    LOG.debug("{} search results found", links.size());
+    for (int uid : expectedUsageId){
+      int speciesKey = parseIdLink(links.remove().getAttribute("href"));
+      assertEquals("Expected result usage id different", speciesKey, uid);
+    }
+  }
+
+}
