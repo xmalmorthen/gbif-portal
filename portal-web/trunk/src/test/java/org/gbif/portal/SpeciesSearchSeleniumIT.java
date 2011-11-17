30e60a4ba845fa392502920bfce70e54f5dd085e
federicomh@gmail.com <federicomh@gmail.com@3650c4fd-ea0f-e2d6-5ead-89630c913e81>
2011-11-17 12:52:57 +0000

    character š changed to the unicode representation U+0161

--- portal-web/trunk/src/test/java/org/gbif/portal/SpeciesSearchSeleniumIT.java
+++ portal-web/trunk/src/test/java/org/gbif/portal/SpeciesSearchSeleniumIT.java
@@ -11,6 +11,62 @@ import static org.junit.Assert.assertNull;
 
 public class SpeciesSearchSeleniumIT extends SeleniumTestBase {
 
+  /**
+   * @param name the q name to search for
+   * @param searchNub true if only the nub is being searched, false to search all
+   * @param expectedUsageId the list of expected name usage ids in the result, starting with first entry
+   */
+  private void assertNameSearch(String name, boolean searchNub, Integer expectedNumResults, int... expectedUsageId) {
+    LOG.debug("Asserting name search '{}'", name);
+    driver.get(getPortalUrl("species"));
+    // Find input form and enter seach text
+    WebElement input = driver.findElement(By.id("q"));
+    input.sendKeys(name);
+    LOG.debug("SEARCH FORM VALUE: {}", input.getAttribute("value"));
+    driver.findElement(By.id("submitSearch")).click();
+
+    // do we want a search across all checklists?
+    if (searchNub) {
+      // TODO: doesnt seem to work. Maybe some javascript fix needed?
+      driver.findElement(By.id("selectedFilters")).findElement(By.cssSelector("a")).click();
+    }
+
+    // main content div on page
+    WebElement content = driver.findElement(By.id("content"));
+
+    // assert number of hits
+    if (expectedNumResults != null) {
+      // TODO: current search does always return all records due to the function query. Activate once solved
+      // assertEquals("Expected number of shown search results wrong", expectedNumResults, (Integer)
+// content.findElements(By.cssSelector("div.result")).size());
+    }
+
+    // assert exact results
+    LinkedList<WebElement> links = new LinkedList(content.findElements(By.cssSelector("div.result h2 a")));
+    LOG.debug("{} search results found", links.size());
+    for (int uid : expectedUsageId) {
+      int speciesKey = parseIdLink(links.remove().getAttribute("href"));
+      assertEquals("Expected result usage id different", speciesKey, uid);
+    }
+  }
+
+  /**
+   * This test uses the dataset page to search for all datasets, clicks on the second one and verifies that
+   * the details page for the selected dataset is shown.
+   */
+  @Test
+  public void testAvesSearch() {
+    assertNameSearch("Aves", true, 20, 212);
+    assertNameSearch("Aves", false, 20, 212);
+    // assert first 10 hits are all Aves
+    LinkedList<WebElement> titles = new LinkedList(driver.findElements(By.cssSelector("div.result h2 a strong")));
+    for (WebElement t : titles) {
+      // TODO: enable this once the remove nub facet click is working in assertNameSearch
+      // LOG.info(t.getText());
+      // assertTrue(t.getText().startsWith("Aves"));
+    }
+  }
+
   @Test
   public void testIdPattern() {
     assertEquals((Integer) 2685464, parseIdLink("http://staging.gbif.org:8080/portal-web-dynamic/species/2685464"));
@@ -25,62 +81,7 @@ public class SpeciesSearchSeleniumIT extends SeleniumTestBase {
   public void testVernacularNameSearch() {
     assertNameSearch("Abies pinsapo ", true, 16, 2685464);
     assertNameSearch("Spanische TANNE", true, 1, 2685464);
-    assertNameSearch("španjolska jela", true, 1, 2685464);
-  }
-
-    /**
-   * This test uses the dataset page to search for all datasets, clicks on the second one and verifies that
-   * the details page for the selected dataset is shown.
-   */
-  @Test
-  public void testAvesSearch() {
-    assertNameSearch("Aves", true, 20, 212);
-    assertNameSearch("Aves", false, 20, 212);
-    // assert first 10 hits are all Aves
-    LinkedList<WebElement> titles = new LinkedList(driver.findElements(By.cssSelector("div.result h2 a strong")));
-    for (WebElement t : titles){
-      //TODO: enable this once the remove nub facet click is working in assertNameSearch
-      //LOG.info(t.getText());
-      //assertTrue(t.getText().startsWith("Aves"));
-    }
-  }
-
-  /**
-   * @param name the q name to search for
-   * @param searchNub true if only the nub is being searched, false to search all
-   * @param expectedUsageId the list of expected name usage ids in the result, starting with first entry
-   */
-  private void assertNameSearch(String name, boolean searchNub, Integer expectedNumResults, int ... expectedUsageId){
-    LOG.debug("Asserting name search '{}'", name);
-    driver.get(getPortalUrl("species"));
-    // Find input form and enter seach text
-    WebElement input = driver.findElement(By.id("q"));
-    input.sendKeys(name);
-    LOG.debug("SEARCH FORM VALUE: {}", input.getAttribute("value"));
-    driver.findElement(By.id("submitSearch")).click();
-
-    // do we want a search across all checklists?
-    if (searchNub) {
-      //TODO: doesnt seem to work. Maybe some javascript fix needed?
-      driver.findElement(By.id("selectedFilters")).findElement(By.cssSelector("a")).click();
-    }
-
-    // main content div on page
-    WebElement content = driver.findElement(By.id("content"));
-
-    // assert number of hits
-    if (expectedNumResults != null){
-      //TODO: current search does always return all records due to the function query. Activate once solved
-      //assertEquals("Expected number of shown search results wrong", expectedNumResults, (Integer) content.findElements(By.cssSelector("div.result")).size());
-    }
-
-    // assert exact results
-    LinkedList<WebElement> links = new LinkedList(content.findElements(By.cssSelector("div.result h2 a")));
-    LOG.debug("{} search results found", links.size());
-    for (int uid : expectedUsageId){
-      int speciesKey = parseIdLink(links.remove().getAttribute("href"));
-      assertEquals("Expected result usage id different", speciesKey, uid);
-    }
+    assertNameSearch("\u0161panjolska jela", true, 1, 2685464);
   }
 
 }
