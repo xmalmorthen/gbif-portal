5f15d790b54d2e09c4ca76f294e86766891d0f11
federicomh@gmail.com <federicomh@gmail.com@3650c4fd-ea0f-e2d6-5ead-89630c913e81>
2011-12-09 11:39:17 +0000

    expected # of results and expected name usage changed for test case

--- portal-web/trunk/src/test/java/org/gbif/portal/SpeciesSearchSeleniumIT.java
+++ portal-web/trunk/src/test/java/org/gbif/portal/SpeciesSearchSeleniumIT.java
@@ -81,7 +81,7 @@ public class SpeciesSearchSeleniumIT extends SeleniumTestBase {
   public void testVernacularNameSearch() {
     assertNameSearch("Abies pinsapo ", true, 16, 2685464);
     // TODO: Spanische TANNE is not a vernacular name valid when using the language filter
-    assertNameSearch("Spanische", true, 1, 2685464);
+    assertNameSearch("Spanische", true, 11, 3898019);
     assertNameSearch("jedla \u0161panielska", true, 1, 2685464);
   }
 
