a540a00d70d22bf89e58427107a2abbb766fbddb
kyle.braak@gmail.com <kyle.braak@gmail.com@3650c4fd-ea0f-e2d6-5ead-89630c913e81>
2011-12-29 22:21:43 +0000

    result no longer uses default, since dataset type is being set now

--- portal-web/trunk/src/test/java/org/gbif/portal/action/dataset/DetailActionTest.java
+++ portal-web/trunk/src/test/java/org/gbif/portal/action/dataset/DetailActionTest.java
@@ -24,11 +24,11 @@ public class DetailActionTest {
     // Dataset title = "OCNMS: Physical Oceanography: moored temperature data: Makah Bay, Washington, USA (MB015)"
     da.setId("7879e569-4a13-4643-b833-d1a564675b86:urn:lsid:knb.ecoinformatics.org:MB015X_015MTBD015R00_20040517:50");
     result = da.execute();
-    assertEquals("detail_checklist", result);
+    assertEquals("detail_external", result);
     // External - escaped
     // Dataset title = "Soil Calcium"
     da.setId("7879e569-4a13-4643-b833-d1a564675b86:urn%3Alsid%3Aknb.ecoinformatics.org%3Aknb-lter-cdr%3A8133");
     result = da.execute();
-    assertEquals("detail_checklist", result);
+    assertEquals("detail_external", result);
   }
 }
