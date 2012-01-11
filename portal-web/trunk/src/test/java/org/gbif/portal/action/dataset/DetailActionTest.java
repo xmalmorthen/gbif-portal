2a45e7cbce962c0642f610fb9136df7419bd220e
kyle.braak@gmail.com <kyle.braak@gmail.com@3650c4fd-ea0f-e2d6-5ead-89630c913e81>
2012-01-11 10:18:52 +0000

    Fixes broken test

--- portal-web/trunk/src/test/java/org/gbif/portal/action/dataset/DetailActionTest.java
+++ portal-web/trunk/src/test/java/org/gbif/portal/action/dataset/DetailActionTest.java
@@ -15,12 +15,10 @@ public class DetailActionTest {
     Injector injector = Guice.createInjector(new PortalModule());
     DetailAction da = injector.getInstance(org.gbif.portal.action.dataset.DetailAction.class);
     assertNotNull(da);
-    // Checklist: "Additional Common Names for the Catalogue of Life"
-    da.setId("f8ef0556-2a27-11e1-bb68-483019ec54e1");
+    // Checklist: "Orthoptera Species File"
+    da.setId("af66d4cf-0fd2-434b-9334-9806a5efa6f7");
     String result = da.execute();
     assertEquals("detail_checklist", result);
-
-    // External - still defaults to Checklist though
     // Dataset title = "OCNMS: Physical Oceanography: moored temperature data: Makah Bay, Washington, USA (MB015)"
     da.setId("7879e569-4a13-4643-b833-d1a564675b86:urn:lsid:knb.ecoinformatics.org:MB015X_015MTBD015R00_20040517:50");
     result = da.execute();
