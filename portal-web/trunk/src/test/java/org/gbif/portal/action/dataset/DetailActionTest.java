b82c85250b7c43a7288a54d1e94e2366a4890554
kyle.braak@gmail.com <kyle.braak@gmail.com@3650c4fd-ea0f-e2d6-5ead-89630c913e81>
2012-01-18 15:55:23 +0000

    Fixes broken test.

--- portal-web/trunk/src/test/java/org/gbif/portal/action/dataset/DetailActionTest.java
+++ portal-web/trunk/src/test/java/org/gbif/portal/action/dataset/DetailActionTest.java
@@ -4,6 +4,7 @@ import org.gbif.portal.config.PortalModule;
 
 import com.google.inject.Guice;
 import com.google.inject.Injector;
+import com.opensymphony.xwork2.Action;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -18,15 +19,15 @@ public class DetailActionTest {
     // Checklist: "Orthoptera Species File"
     da.setId("af66d4cf-0fd2-434b-9334-9806a5efa6f7");
     String result = da.execute();
-    assertEquals("detail", result);
+    assertEquals(Action.SUCCESS, result);
     // Dataset title = "OCNMS: Physical Oceanography: moored temperature data: Makah Bay, Washington, USA (MB015)"
     da.setId("7879e569-4a13-4643-b833-d1a564675b86:urn:lsid:knb.ecoinformatics.org:MB015X_015MTBD015R00_20040517:50");
     result = da.execute();
-    assertEquals("detail", result);
+    assertEquals(Action.SUCCESS, result);
     // External - escaped
     // Dataset title = "Soil Calcium"
     da.setId("7879e569-4a13-4643-b833-d1a564675b86:urn%3Alsid%3Aknb.ecoinformatics.org%3Aknb-lter-cdr%3A8133");
     result = da.execute();
-    assertEquals("detail", result);
+    assertEquals(Action.SUCCESS, result);
   }
 }
