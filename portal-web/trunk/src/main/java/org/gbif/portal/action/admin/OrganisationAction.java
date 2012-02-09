4578ea027e07071158041595f8a620c7f14acc32
wixner <wixner@3650c4fd-ea0f-e2d6-5ead-89630c913e81>
2012-02-09 16:31:46 +0000

    Adding blank org & dataset admin actions

---
+++ portal-web/trunk/src/main/java/org/gbif/portal/action/admin/OrganisationAction.java
@@ -0,0 +1,11 @@
+package org.gbif.portal.action.admin;
+
+import org.gbif.portal.action.BaseAction;
+
+public class OrganisationAction extends BaseAction {
+
+  @Override
+  public String execute() {
+    return SUCCESS;
+  }
+}
