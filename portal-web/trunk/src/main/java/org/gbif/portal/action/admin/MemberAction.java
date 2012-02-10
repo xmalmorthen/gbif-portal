f553ab8bd81b3c230001f4c5e07871304a769d14
wixner <wixner@3650c4fd-ea0f-e2d6-5ead-89630c913e81>
2012-02-10 10:14:41 +0000

    Rename org admin action to member

--- portal-web/trunk/src/main/java/org/gbif/portal/action/admin/MemberAction.java
+++ portal-web/trunk/src/main/java/org/gbif/portal/action/admin/MemberAction.java
@@ -2,7 +2,7 @@ package org.gbif.portal.action.admin;
 
 import org.gbif.portal.action.BaseAction;
 
-public class OrganisationAction extends BaseAction {
+public class MemberAction extends BaseAction {
 
   @Override
   public String execute() {
