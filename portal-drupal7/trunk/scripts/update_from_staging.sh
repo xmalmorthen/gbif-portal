3d701d9baad0df5e89a0cebf82f1882696eb9acc
wixner <wixner@3650c4fd-ea0f-e2d6-5ead-89630c913e81>
2012-04-04 13:32:50 +0000

    use devel readonly account

--- portal-drupal7/trunk/scripts/update_from_staging.sh
+++ portal-drupal7/trunk/scripts/update_from_staging.sh
@@ -1,3 +1,3 @@
-mysqldump -u devel -pXYZ -h nerf.gbif.org drupal_w2 > ~/portal-drupal.dump
+mysqldump -u drupal_w2_ro -pTRQm5lF2 -h nerf.gbif.org drupal_w2 > ~/portal-drupal.dump
 mysql -u root -e "drop database drupal; create database drupal"
 mysql -u root drupal < portal-drupal.dump
