9c8cc1f48660ff47c10e5e0419ea19185b4383eb
wixner <wixner@3650c4fd-ea0f-e2d6-5ead-89630c913e81>
2012-04-04 13:41:55 +0000

    use tmp as dump location

--- portal-drupal7/trunk/scripts/update_from_staging.sh
+++ portal-drupal7/trunk/scripts/update_from_staging.sh
@@ -1,3 +1,3 @@
-mysqldump -u drupal_w2_ro -pTRQm5lF2 -h nerf.gbif.org drupal_w2 > ~/portal-drupal.dump
+mysqldump -u drupal_w2_ro -pTRQm5lF2 -h nerf.gbif.org drupal_w2 > /tmp/portal-drupal.dump
 mysql -u root -e "drop database drupal; create database drupal"
-mysql -u root drupal < portal-drupal.dump
+mysql -u root drupal < /tmp/portal-drupal.dump
