d27d6048f80ad611ada2abe856a94da2c46793de
wixner <wixner@3650c4fd-ea0f-e2d6-5ead-89630c913e81>
2012-04-05 12:42:16 +0000

    Modify cas_module to always also insert a cas user name

--- portal-drupal7/trunk/scripts/update_from_staging.sh
+++ portal-drupal7/trunk/scripts/update_from_staging.sh
@@ -1,3 +1,6 @@
-mysqldump -u drupal_w2_ro -pTRQm5lF2 -h nerf.gbif.org drupal_w2 > /tmp/portal-drupal.dump
+echo "Dumping staging DB"
+mysqldump -u drupal_w2 -pGW49mslFZ -h nerf.gbif.org drupal_w2 > /tmp/portal-drupal.dump
+echo "Delete local DB"
 mysql -u root -e "drop database drupal; create database drupal"
+echo "Import into local DB"
 mysql -u root drupal < /tmp/portal-drupal.dump
