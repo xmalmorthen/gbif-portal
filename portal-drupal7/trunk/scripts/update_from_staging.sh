9cee3e0a7d8d6a31f4196f17c54d55d24d3574e9
wixner <wixner@3650c4fd-ea0f-e2d6-5ead-89630c913e81>
2012-04-05 12:45:21 +0000

    revert unintentional committs

--- portal-drupal7/trunk/scripts/update_from_staging.sh
+++ portal-drupal7/trunk/scripts/update_from_staging.sh
@@ -1,5 +1,5 @@
 echo "Dumping staging DB"
-mysqldump -u drupal_w2 -pGW49mslFZ -h nerf.gbif.org drupal_w2 > /tmp/portal-drupal.dump
+mysqldump -u drupal_w2_ro -pTRQm5lF2 -h nerf.gbif.org drupal_w2 > /tmp/portal-drupal.dump
 echo "Delete local DB"
 mysql -u root -e "drop database drupal; create database drupal"
 echo "Import into local DB"
