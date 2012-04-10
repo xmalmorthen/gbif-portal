c5969b7c8613b04a4c95ce4ab2a966c136374571
bko@gbif.org <bko@gbif.org@3650c4fd-ea0f-e2d6-5ead-89630c913e81>
2012-04-10 08:20:07 +0000

    Added GBIF2012 theme and its basetheme Omega.

---
+++ portal-drupal7/trunk/sites/all/themes/gbif2012/templates/region--user_second.tpl.php
@@ -0,0 +1,8 @@
+<aside<?php print $attributes; ?>>
+  <div<?php print $content_attributes; ?>>
+    <a href="/user">Log in</a> or <a href="/user/register">Create a new account</a>
+    <?php
+      print $content;
+    ?>
+  </div>
+</aside>
