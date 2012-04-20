54ebc812989874e9b6726010a6baece0b27c7a8c
wixner <wixner@3650c4fd-ea0f-e2d6-5ead-89630c913e81>
2012-04-20 15:32:52 +0000

    Adding real test

--- drupal-mybatis/trunk/src/test/java/org/gbif/user/mybatis/UserServiceImplIT.java
+++ drupal-mybatis/trunk/src/test/java/org/gbif/user/mybatis/UserServiceImplIT.java
@@ -1,15 +1,25 @@
 package org.gbif.user.mybatis;
 
+import org.gbif.api.model.User;
+import org.gbif.user.UserService;
+
+import com.google.inject.Inject;
+import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 
 public class UserServiceImplIT {
+
+  @Inject
+  private UserService service;
 
   @Rule
   public DrupalTestRule ddt = new DrupalTestRule("users.xml");
 
   @Test
   public void testGet() throws Exception {
-
+    User admin = service.get("admin");
+    Assert.assertNotNull(admin);
+    Assert.assertEquals("admin", admin.getName());
   }
 }
