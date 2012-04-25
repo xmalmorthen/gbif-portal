9eff5057522988f3cffdda8ffc509f7916d29093
wixner <wixner@3650c4fd-ea0f-e2d6-5ead-89630c913e81>
2012-04-25 15:10:32 +0000

    Use very short lived cache (TTL = 1 minute) for users so that applications doing batch work dont have to query a user for every single request

--- drupal-mybatis/trunk/src/main/java/org/gbif/user/mybatis/UserServiceImpl.java
+++ drupal-mybatis/trunk/src/main/java/org/gbif/user/mybatis/UserServiceImpl.java
@@ -3,19 +3,38 @@ package org.gbif.user.mybatis;
 import org.gbif.api.model.User;
 import org.gbif.api.service.UserService;
 
+import java.util.concurrent.TimeUnit;
+
+import com.google.common.base.Strings;
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
 import com.google.inject.Inject;
 
 public class UserServiceImpl implements UserService {
   private UserMapper mapper;
+  private Cache<String, User> cache;
 
   @Inject
-  UserServiceImpl(UserMapper mapper) {
+  public UserServiceImpl(UserMapper mapper) {
     this.mapper = mapper;
+    cache = CacheBuilder.newBuilder()
+      .maximumSize(1000)
+      .expireAfterWrite(1, TimeUnit.MINUTES)
+      .build();
   }
 
   @Override
   public User get(String username) {
-    return mapper.get(username);
+    if (Strings.isNullOrEmpty(username)){
+      return null;
+    }
+
+    User u = cache.getIfPresent(username);
+    if (u == null){
+      u = mapper.get(username);
+      cache.put(username, u);
+    }
+    return u;
   }
 
 }
