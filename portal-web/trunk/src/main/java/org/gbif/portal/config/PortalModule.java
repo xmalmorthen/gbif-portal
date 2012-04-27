8b67078154408b031b971103e0ca5871ba9b3d17
wixner <wixner@3650c4fd-ea0f-e2d6-5ead-89630c913e81>
2012-04-27 15:01:33 +0000

    Create a private guice module for the portal too so its not interferring with the others

--- portal-web/trunk/src/main/java/org/gbif/portal/config/PortalModule.java
+++ portal-web/trunk/src/main/java/org/gbif/portal/config/PortalModule.java
@@ -4,58 +4,20 @@ import org.gbif.checklistbank.ws.client.guice.ChecklistBankWsClientModule;
 import org.gbif.occurrencestore.ws.client.guice.OccurrenceWsClientModule;
 import org.gbif.registry.ws.client.guice.RegistryWsClientModule;
 import org.gbif.user.guice.DrupalMyBatisModule;
-import org.gbif.utils.HttpUtil;
-import org.gbif.ws.client.filter.HttpGbifAuthFilter;
+import org.gbif.ws.util.PropertiesUtil;
 
-import java.io.IOException;
 import java.util.Properties;
 
 import com.google.inject.AbstractModule;
-import com.google.inject.Inject;
-import com.google.inject.Provides;
-import com.google.inject.Scopes;
-import com.google.inject.Singleton;
-import com.google.inject.name.Named;
-import com.google.inject.name.Names;
-import com.sun.jersey.api.client.filter.ClientFilter;
-import org.apache.http.client.HttpClient;
 
 public class PortalModule extends AbstractModule {
-
-  /**
-   * The portal module combines all service modules needed for the portal.
-   * You can switch between the mybatis and the ws-client api implementation for checklistbank
-   * by swapping the used module below.
-   * When mybatis is used make sure your maven settings include the checklistbank jdbc properties.
-   * 
-   * @throws ConfigurationException If the application properties cannot be read
-   */
-  private Properties bindApplicationProperties() throws ConfigurationException {
-    try {
-      // load and bind single properties to pass on to other modules.
-      Properties properties = new Properties();
-      properties.load(this.getClass().getResourceAsStream("/application.properties"));
-      Names.bindProperties(binder(), properties);
-      return properties;
-    } catch (IOException e) {
-      throw new ConfigurationException(
-        "Unable to read the application.properties (perhaps missing in WEB-INF/classes?)", e);
-    }
-  }
-
-  @Provides
-  @Singleton
-  @Inject
-  public ClientFilter provideSessionAuthFilter(SessionAuthProvider sessionAuthProvider,
-    @Named("application.key") String applicationKey) {
-    return new HttpGbifAuthFilter(applicationKey, sessionAuthProvider);
-  }
+  private static final String PROPERTIES_FILE = "application.properties";
 
   @Override
   protected void configure() {
-    Properties properties = bindApplicationProperties();
+    Properties properties = PropertiesUtil.readFromClasspath(PROPERTIES_FILE);
 
-    bind(SessionAuthProvider.class).in(Scopes.SINGLETON);
+    install(new PrivatePortalModule(properties));
 
     // bind registry API
     install(new RegistryWsClientModule(properties, true, true));
@@ -69,12 +31,5 @@ public class PortalModule extends AbstractModule {
     // bind occurrence API
     install(new OccurrenceWsClientModule(properties));
   }
-
-  @Provides
-  @Singleton
-  public HttpClient provideHttpClient() {
-    return HttpUtil.newMultithreadedClient();
-  }
-
 
 }
---
+++ portal-web/trunk/src/main/java/org/gbif/portal/config/PrivatePortalModule.java
@@ -0,0 +1,38 @@
+package org.gbif.portal.config;
+
+import org.gbif.ws.client.filter.HttpGbifAuthFilter;
+import org.gbif.ws.server.filter.AuthFilter;
+
+import java.util.Properties;
+
+import com.google.inject.Inject;
+import com.google.inject.PrivateModule;
+import com.google.inject.Provides;
+import com.google.inject.Scopes;
+import com.google.inject.Singleton;
+import com.google.inject.name.Names;
+import com.sun.jersey.api.client.filter.ClientFilter;
+
+public class PrivatePortalModule extends PrivateModule{
+  private final Properties properties;
+
+  public PrivatePortalModule(Properties properties) {
+    this.properties = properties;
+  }
+
+  @Override
+  protected void configure() {
+    Names.bindProperties(binder(), properties);
+
+    bind(SessionAuthProvider.class).in(Scopes.SINGLETON);
+
+    expose(ClientFilter.class);
+  }
+
+  @Provides
+  @Singleton
+  @Inject
+  public ClientFilter provideSessionAuthFilter(SessionAuthProvider sessionAuthProvider) {
+    return new HttpGbifAuthFilter(AuthFilter.PORTAL_APP_KEY, sessionAuthProvider);
+  }
+}
