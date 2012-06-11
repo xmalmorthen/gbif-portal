94f107bee60922c72797c15b7d18fb9d5d2dc554
wixner <wixner@3650c4fd-ea0f-e2d6-5ead-89630c913e81>
2012-06-11 12:55:53 +0000

    Throwing explicit NPE when user cannot be found in drupal

--- drupal-mybatis/trunk/src/main/java/org/gbif/user/mybatis/UserServiceImpl.java
+++ drupal-mybatis/trunk/src/main/java/org/gbif/user/mybatis/UserServiceImpl.java
@@ -12,6 +12,7 @@ import com.google.common.cache.CacheBuilder;
 import com.google.inject.Inject;
 
 public class UserServiceImpl implements UserService {
+
   private UserMapper mapper;
   private Cache<String, User> cache;
   private PasswordEncoder encoder = new PasswordEncoder("MD5");
@@ -19,21 +20,22 @@ public class UserServiceImpl implements UserService {
   @Inject
   public UserServiceImpl(UserMapper mapper) {
     this.mapper = mapper;
-    cache = CacheBuilder.newBuilder()
-      .maximumSize(1000)
-      .expireAfterWrite(1, TimeUnit.MINUTES)
-      .build();
+    cache = CacheBuilder.newBuilder().maximumSize(1000).expireAfterWrite(1, TimeUnit.MINUTES).build();
   }
 
   @Override
   public User get(String username) {
-    if (Strings.isNullOrEmpty(username)){
+    if (Strings.isNullOrEmpty(username)) {
       return null;
     }
 
     User u = cache.getIfPresent(username);
-    if (u == null){
+    if (u == null) {
       u = mapper.get(username);
+      // make sure we found a user with that name
+      if (u == null) {
+        throw new NullPointerException("Cannot find user " + username);
+      }
       cache.put(username, u);
     }
     return u;
@@ -41,15 +43,15 @@ public class UserServiceImpl implements UserService {
 
   @Override
   public User authenticate(String username, String password) {
-    if (Strings.isNullOrEmpty(username) || password == null){
+    if (Strings.isNullOrEmpty(username) || password == null) {
       return null;
     }
 
     User u = get(username);
-    if (u != null){
+    if (u != null) {
       // build password hash stored in drupal
       String phash = encoder.encode(password);
-      if (phash.equalsIgnoreCase(u.getPasswordHash())){
+      if (phash.equalsIgnoreCase(u.getPasswordHash())) {
         return u;
       }
     }
