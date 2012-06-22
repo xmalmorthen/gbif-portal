fa4f94e8ec896e3df8f66238a7fb86c6da95943e
wixner <wixner@3650c4fd-ea0f-e2d6-5ead-89630c913e81>
2012-06-22 15:21:41 +0000

    Cleaning app properties, removing solr, nub match entries and hardcode the species suggest ws url in Config cause all our searches implement also the suggest service

--- portal-web/trunk/src/main/java/org/gbif/portal/config/Config.java
+++ portal-web/trunk/src/main/java/org/gbif/portal/config/Config.java
@@ -11,6 +11,7 @@ import org.slf4j.LoggerFactory;
  * Simple configuration bean to pass the guice binded properties on to the rendering layer.
  */
 public class Config {
+
   private final static Logger LOG = LoggerFactory.getLogger(Config.class);
   public final static String SERVERNAME = "servername";
 
@@ -25,44 +26,45 @@ public class Config {
   private String wsOccSearch;
   private String wsClbSuggest;
 
-  private static String getPropertyUrl(Properties properties, String propName){
+  private static String getPropertyUrl(Properties properties, String propName) {
     String value = null;
     try {
       value = properties.getProperty(propName);
       URI uri = URI.create(value);
       return uri.toString();
     } catch (Exception e) {
-      throw new ConfigurationException(value+" is no valid URL for property "+propName+". Please configure application.properties appropriately!", e);
+      throw new ConfigurationException(value + " is no valid URL for property " + propName
+                                       + ". Please configure application.properties appropriately!", e);
     }
   }
 
-  public static Config buildFromProperties(){
+  public static Config buildFromProperties() {
     Config cfg = new Config();
+    try {
+      Properties properties = new Properties();
+      properties.load(Config.class.getResourceAsStream("/application.properties"));
+      // prefer system variable if existing
       try {
-        Properties properties = new Properties();
-        properties.load(Config.class.getResourceAsStream("/application.properties"));
-        // prefer system variable if existing
-        try {
-          URI uri = URI.create(System.getProperty(SERVERNAME));
-          cfg.serverName = uri.toString();
-          LOG.debug("Using servername system variable");
-        } catch (Exception e) {
-          cfg.serverName = getPropertyUrl(properties, SERVERNAME);
-        }
-        LOG.debug("Setting servername to {}", cfg.serverName);
-
-        cfg.cas = getPropertyUrl(properties, "cas.url");
-        cfg.drupal = getPropertyUrl(properties, "drupal.url");
-        cfg.wsClb = getPropertyUrl(properties, "checklistbank.ws.url");
-        cfg.wsClbSearch = getPropertyUrl(properties, "checklistbank.search.ws.url");
-        cfg.wsReg = getPropertyUrl(properties, "registry.ws.url");
-        cfg.wsRegSearch = getPropertyUrl(properties, "registry.search.ws.url");
-        cfg.wsOcc = getPropertyUrl(properties, "occurrencestore.ws.url");
-        cfg.wsOccSearch = getPropertyUrl(properties, "occurrencestore.search.ws.url");
-        cfg.wsClbSuggest = getPropertyUrl(properties, "checklistbank.suggest.ws.url");
-      } catch (IOException e) {
-        throw new ConfigurationException("application.properties cannot be read", e);
+        URI uri = URI.create(System.getProperty(SERVERNAME));
+        cfg.serverName = uri.toString();
+        LOG.debug("Using servername system variable");
+      } catch (Exception e) {
+        cfg.serverName = getPropertyUrl(properties, SERVERNAME);
       }
+      LOG.debug("Setting servername to {}", cfg.serverName);
+
+      cfg.cas = getPropertyUrl(properties, "cas.url");
+      cfg.drupal = getPropertyUrl(properties, "drupal.url");
+      cfg.wsClb = getPropertyUrl(properties, "checklistbank.ws.url");
+      cfg.wsClbSearch = getPropertyUrl(properties, "checklistbank.search.ws.url");
+      cfg.wsClbSuggest = cfg.wsClbSearch + "search/suggest";
+      cfg.wsReg = getPropertyUrl(properties, "registry.ws.url");
+      cfg.wsRegSearch = getPropertyUrl(properties, "registry.search.ws.url");
+      cfg.wsOcc = getPropertyUrl(properties, "occurrencestore.ws.url");
+      cfg.wsOccSearch = getPropertyUrl(properties, "occurrencestore.search.ws.url");
+    } catch (IOException e) {
+      throw new ConfigurationException("application.properties cannot be read", e);
+    }
     return cfg;
   }
 
