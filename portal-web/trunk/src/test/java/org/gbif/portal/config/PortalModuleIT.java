1b05d1032be557983cdc67ff5bcf001553b0b8b3
kyle.braak@gmail.com <kyle.braak@gmail.com@3650c4fd-ea0f-e2d6-5ead-89630c913e81>
2012-12-19 13:26:12 +0000

    Test renamed according to comment in http://dev.gbif.org/issues/browse/GBIFCOM-106. Test extended by single test. Work relates specifically to http://dev.gbif.org/issues/browse/POR-399.

--- portal-web/trunk/src/test/java/org/gbif/portal/config/PortalModuleIT.java
+++ portal-web/trunk/src/test/java/org/gbif/portal/config/PortalModuleIT.java
@@ -1,7 +1,9 @@
 package org.gbif.portal.config;
 
 import org.gbif.api.model.checklistbank.DatasetMetrics;
+import org.gbif.api.model.registry.Dataset;
 import org.gbif.api.service.checklistbank.DatasetMetricsService;
+import org.gbif.api.service.registry.DatasetService;
 import org.gbif.portal.action.ActionTestUtil;
 
 import java.util.UUID;
@@ -12,7 +14,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertNull;
 
-public class PortalModuleTest {
+public class PortalModuleIT {
 
   private Injector injector;
 
@@ -41,5 +43,27 @@ public class PortalModuleTest {
     DatasetMetricsService cms = injector.getInstance(org.gbif.api.service.checklistbank.DatasetMetricsService.class);
     DatasetMetrics metrics = cms.get(UUID.randomUUID());
     assertNull(metrics);
+  }
+
+  /**
+   * This test tries to retrieve a dataset calling: public Dataset get(@PathParam("key") String key).
+   * </p>
+   * The ws-client method is annotated with @NullForNotFound (a client interceptor called NotFoundToNullInterceptor
+   * catches a NotFoundException and returns NULL instead). The ws-client method is also annotated with
+   * HttpErrorResponseInterceptor (a client method interceptor that translates http response errors into actual java
+   * exceptions. The order of the interceptors is important, so that the 404 gets translated into a NotFoundException
+   * and this gets translated into null.
+   * </p>
+   * This test intentionally uses a dataset that doesn't exist. The web service returns a 404 (not found) response.
+   * If the NotFoundToNullInterceptor and HttpErrorResponseInterceptor have been bound, and the interceptor order is
+   * correct, the final response is NULL.
+   * @see org.gbif.ws.client.interceptor.NotFoundToNullInterceptor
+   * @see org.gbif.ws.client.interceptor.HttpErrorResponseInterceptor#invoke(org.aopalliance.intercept.MethodInvocation)
+   */
+  @Test
+  public void testRegistryWsClientModuleInstalled() {
+    DatasetService ds = injector.getInstance(org.gbif.api.service.registry.DatasetService.class);
+    Dataset dataset = ds.get(UUID.randomUUID());
+    assertNull(dataset);
   }
 }
