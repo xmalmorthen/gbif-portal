042fe5f9b75e7bb73844ca79a63ad7c14b68a409
wixner <wixner@3650c4fd-ea0f-e2d6-5ead-89630c913e81>
2013-02-06 17:07:12 +0000

    [maven-release-plugin]  copy for tag drupal-mybatis-0.3

---
+++ drupal-mybatis/tags/drupal-mybatis-0.3/src/main/java/org/gbif/user/guice/DrupalMyBatisModule.java
@@ -0,0 +1,33 @@
+package org.gbif.user.guice;
+
+import org.gbif.api.service.common.UserService;
+import org.gbif.service.guice.PrivateServiceModule;
+
+import java.util.Properties;
+
+public class DrupalMyBatisModule extends PrivateServiceModule {
+
+  private static final String PREFIX = "drupal.db.";
+
+  /**
+   * Uses the given properties to configure the service.
+   * 
+   * @param properties to use
+   */
+  public DrupalMyBatisModule(Properties properties) {
+    super(PREFIX, properties);
+  }
+
+  @Override
+  protected void configureService() {
+    // bind classes
+    InternalDrupalMyBatisModule mod = new InternalDrupalMyBatisModule();
+    install(mod);
+
+    // expose named datasource binding
+    expose(mod.getDatasourceKey());
+
+    // expose services
+    expose(UserService.class);
+  }
+}
