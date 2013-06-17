333970a211c1d590a252ab83256c21bdbf89b592
gbif.webm <gbif.webm@3650c4fd-ea0f-e2d6-5ead-89630c913e81>
2013-06-17 16:13:01 +0000

    remove unused theme folders

--- portal-drupal7/trunk/sites/all/themes/gbif2012/templates/region--credit_first.tpl.php
+++
@@ -1,8 +0,0 @@
-<aside<?php print $attributes; ?>>
-  <div<?php print $content_attributes; ?>>
-    <p>&copy;2012 <a href="www.gbif.org">Global Biodiversity Information Facility</a>. Data publishers retain all rights to data.</p>
-    <?php
-      print $content;
-    ?>
-  </div>
-</aside>
