56c9d4ff200774cc7dedac5dc34c2e6a2181c9af
federicomh@gmail.com <federicomh@gmail.com@3650c4fd-ea0f-e2d6-5ead-89630c913e81>
2013-06-25 15:18:45 +0000

    sensitive steps that create and move solr indexes moved to copy-indexes.sh

--- portal-web/trunk/scripts/deploy.sh
+++ portal-web/trunk/scripts/deploy.sh
@@ -2,15 +2,6 @@
 mkdir $ENV
 cd $ENV
 
-# copy the metadata from staging to $ENV
-scp -r root@staging:/usr/local/share/registry-metadata/metadata .
-scp -r metadata/* root@jawa:/var/local/large/metadata
-
-#Occurrence solr index was built on jawa //mnt/ssd/solr/occurrence-index-builder/solr
-#Create a backup of the occurrence index
-ssh root@jawa 'mv //mnt/ssd/solr/occurrence-solr //var/local/large/solr/occurrence-solr$(date +"%b-%d-%Y")'
-ssh root@jawa 'mv //mnt/ssd/solr/occurrence-index-builder/solr //mnt/ssd/solr/occurrence-solr'
-
 # set up the solr installations
 svn checkout https://gbif-common-resources.googlecode.com/svn/solr-web/trunk solr-web
 cd solr-web
