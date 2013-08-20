54b749c211ac6f19752245f1433dc2217687c3ca
gbif.webm <gbif.webm@3650c4fd-ea0f-e2d6-5ead-89630c913e81>
2013-08-20 14:33:53 +0000

    small fix upper menu

--- portal-drupal7/trunk/sites/all/themes/vizz2/template.php
+++ portal-drupal7/trunk/sites/all/themes/vizz2/template.php
@@ -825,8 +825,8 @@ echo '        <div class="about">
           <ul> ' ;
 echo "            <li><a href='$base_url/whatisgbif'>What is GBIF</a></li> " ;
 echo "            <li><a href='$base_url/resources/keyinformation'>Key information</a></li> " ;
-echo "            <li><a href='$base_url/governamce/summary'>Governance</a></li> " ;
-echo "            <li><a href='$base_url/whoweworkwith/whoweworkwith/'>Who we work with</a></li> " ;
+echo "            <li><a href='$base_url/governance/summary'>Governance</a></li> " ;
+echo "            <li><a href='$base_url/whoweworkwith'>Who we work with</a></li> " ;
 echo "            <li><a href='$base_url/contact/contactus'>Contact</a></li> " ;
 echo "          </ul>
         </div>
