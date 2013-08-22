af5e7be0b542b9b465ae3a1b33a01e1c439fdde9
gbif.webm <gbif.webm@3650c4fd-ea0f-e2d6-5ead-89630c913e81>
2013-08-22 19:08:08 +0000

    get "beta" image from $dataportal_base_url instead of hardcoded value

--- portal-drupal7/trunk/sites/all/themes/vizz2/templates/page.tpl.php
+++ portal-drupal7/trunk/sites/all/themes/vizz2/templates/page.tpl.php
@@ -43,7 +43,7 @@
       </div>
 
 		<a id="disclaimerToggle" href="/portal/disclaimer">
-		<img id="beta" src="http://uat.gbif.org/img/beta.gif">
+		<img id="beta" src="<?php echo $dataportal_base_url?>/img/beta.gif">
 		</a>
 	<?php get_nav($base_url) ?>
     </div>
