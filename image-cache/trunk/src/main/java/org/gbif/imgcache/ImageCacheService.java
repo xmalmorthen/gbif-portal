201b7e524d95ee2e6ec7116bf2888ef2861ccc2d
federicomh@gmail.com <federicomh@gmail.com@3650c4fd-ea0f-e2d6-5ead-89630c913e81>
2013-09-09 06:46:02 +0000

    http://dev.gbif.org/code/cru/CR-POR-146

--- image-cache/trunk/src/main/java/org/gbif/imgcache/ImageCacheService.java
+++ image-cache/trunk/src/main/java/org/gbif/imgcache/ImageCacheService.java
@@ -1,5 +1,6 @@
 package org.gbif.imgcache;
 
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -8,29 +9,35 @@ import java.io.OutputStream;
 import java.io.UnsupportedEncodingException;
 import java.net.URL;
 import java.net.URLEncoder;
+
+import javax.imageio.ImageIO;
 import javax.inject.Inject;
 
 import com.google.common.base.Preconditions;
 import com.google.common.io.ByteStreams;
-import com.google.common.io.Closeables;
+import com.google.common.io.Closer;
 import com.google.inject.name.Named;
 import net.coobird.thumbnailator.Thumbnails;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ImageCacheService {
+
   private final Logger LOG = LoggerFactory.getLogger(ImageCacheService.class);
 
   private final File repo;
-  private final String ENC = "UTF8";
-  private final String format = "png";
+  private static final String ENC = "UTF8";
+  private static final String PNG_FMT = "png";
+  private static final String MIME_TYPE = "image/" + PNG_FMT;
+  private static final String DFT_FILENAME = "image";
 
   @Inject
   public ImageCacheService(@Named("imgcache.repository") String repository) {
     repo = new File(repository);
     LOG.info("Use image repository " + repo.getAbsolutePath());
     if (!repo.exists() && !repo.isDirectory()) {
-      throw new IllegalStateException("imgcache.repository needs to be an existing, writable directory: " + repo.getAbsolutePath());
+      throw new IllegalStateException("imgcache.repository needs to be an existing, writable directory: "
+        + repo.getAbsolutePath());
     }
   }
 
@@ -41,8 +48,36 @@ public class ImageCacheService {
     if (!imgFile.exists()) {
       cacheImage(url);
     }
-    //TODO: store mime type or deduct from image/file suffix
-    return new CachedImage(url, size, "image/" + format, imgFile);
+    // TODO: store mime type or deduct from image/file suffix
+    return new CachedImage(url, size, MIME_TYPE, imgFile);
+  }
+
+  private void cacheImage(URL url) throws IOException {
+    // download original
+    LOG.info("Caching image " + url);
+    File origImg = location(url, ImageSize.ORIGINAL);
+    // create parent folder that is unque for the original image
+    origImg.getParentFile().mkdir();
+
+    OutputStream out = null;
+    InputStream source = null;
+    Closer closer = Closer.create();
+
+    try {
+      out = new FileOutputStream(origImg);
+      closer.register(out);
+      source = url.openStream();
+      closer.register(source);
+      ByteStreams.copy(source, out);
+    } finally {
+      closer.close();
+    }
+
+    // now produce a thumbnail from the original
+    produceImage(url, ImageSize.THUMBNAIL);
+    produceImage(url, ImageSize.SMALL);
+    produceImage(url, ImageSize.MIDSIZE);
+    produceImage(url, ImageSize.LARGE);
   }
 
   private File location(URL url, ImageSize size) throws IOException {
@@ -58,52 +93,26 @@ public class ImageCacheService {
     try {
       fileName = new File(url.getPath()).getName();
     } catch (Exception e) {
-      fileName = "image";
+      fileName = DFT_FILENAME;
     }
 
     String suffix;
-    if (size == ImageSize.ORIGINAL){
+    if (size == ImageSize.ORIGINAL) {
       suffix = "";
     } else {
-      suffix = "-" + size.name().charAt(0) + "." + format;
+      suffix = '-' + size.name().charAt(0) + '.' + PNG_FMT;
     }
 
     return new File(folder, fileName + suffix);
   }
 
-  private void cacheImage(URL url) throws IOException {
-    // download original
-    LOG.info("Caching image " + url);
-    File origImg = location(url, ImageSize.ORIGINAL);
-    // create parent folder that is unque for the original image
-    origImg.getParentFile().mkdir();
-
-    OutputStream out = null;
-    InputStream source = null;
-
-    try {
-      out = new FileOutputStream(origImg);
-      source = url.openStream();
-      ByteStreams.copy(source, out);
-
-    } finally {
-      Closeables.closeQuietly(out);
-      Closeables.closeQuietly(source);
-    }
-
-    // now produce a thumbnail from the original
-    produceImage(url, ImageSize.THUMBNAIL);
-    produceImage(url, ImageSize.SMALL);
-    produceImage(url, ImageSize.MIDSIZE);
-    produceImage(url, ImageSize.LARGE);
-  }
-
   private void produceImage(URL url, ImageSize size) throws IOException {
     File orig = location(url, ImageSize.ORIGINAL);
     File calc = location(url, size);
-    Thumbnails.Builder<File> thumb = Thumbnails.of(orig)
-            .size(size.width, size.height)
-            .outputFormat(format);
+    BufferedImage bufferedImage = ImageIO.read(orig);
+    Thumbnails.Builder<BufferedImage> thumb = Thumbnails.of(bufferedImage)
+      .size(size.width, size.height)
+      .outputFormat(PNG_FMT);
 
     // make sure thumbnails have the same square size
     if (ImageSize.THUMBNAIL == size) {
@@ -112,5 +121,6 @@ public class ImageCacheService {
 
     // process
     thumb.toFile(calc);
+    bufferedImage.flush();
   }
 }
