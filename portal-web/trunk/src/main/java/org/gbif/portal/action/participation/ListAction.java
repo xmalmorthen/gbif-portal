c7d389ea4e9a8dbc4f6c7ffdbe211867f60d118e
wixner <wixner@3650c4fd-ea0f-e2d6-5ead-89630c913e81>
2013-09-18 08:46:46 +0000

    Use full node list

--- portal-web/trunk/src/main/java/org/gbif/portal/action/participation/ListAction.java
+++ portal-web/trunk/src/main/java/org/gbif/portal/action/participation/ListAction.java
@@ -5,14 +5,15 @@ import org.gbif.api.model.common.paging.PagingResponse;
 import org.gbif.api.model.registry.Node;
 import org.gbif.api.service.registry.NodeService;
 import org.gbif.api.vocabulary.NodeType;
+import org.gbif.api.vocabulary.ParticipationStatus;
 import org.gbif.portal.action.BaseAction;
 
 import java.util.List;
 import javax.annotation.Nullable;
 
 import com.google.common.base.Function;
-import com.google.common.base.Predicate;
 import com.google.common.collect.FluentIterable;
+import com.google.common.collect.Lists;
 import com.google.common.collect.Ordering;
 import com.google.inject.Inject;
 
@@ -21,35 +22,36 @@ import com.google.inject.Inject;
  */
 public class ListAction extends BaseAction {
 
-  private List<Node> voting;
-  private List<Node> associate;
-  private List<Node> other;
+  private List<Node> voting = Lists.newArrayList();
+  private List<Node> associate = Lists.newArrayList();
+  private List<Node> other = Lists.newArrayList();
 
   @Inject
   private NodeService nodeService;
 
   @Override
   public String execute() throws Exception {
-    PagingResponse<Node> resp = nodeService.list(new PagingRequest(0, 5));
-    associate = resp.getResults();
-    other = resp.getResults();
-    voting = FluentIterable.from(resp.getResults())
-      // filter out country nodes
-      .filter(new Predicate<Node>() {
-        @Override
-        public boolean apply(@Nullable Node n) {
-          return NodeType.COUNTRY != n.getType();
+    PagingResponse<Node> resp = nodeService.list(new PagingRequest(0, 1000));
+    List<Node> sorted = FluentIterable.from(resp.getResults())
+        // sort alphabetically
+        .toSortedList(Ordering.natural().onResultOf(new Function<Node, String>() {
+          @Nullable
+          @Override
+          public String apply(@Nullable Node n) {
+            return n == null ? null : n.getTitle();
+          }
+    }));
+    for (Node n: sorted) {
+      if (NodeType.COUNTRY == n.getType()) {
+        if (ParticipationStatus.VOTING == n.getParticipationStatus()) {
+          voting.add(n);
+        } else if (ParticipationStatus.ASSOCIATE == n.getParticipationStatus()) {
+          associate.add(n);
         }
-      })
-
-      // sort alphabetically
-      .toSortedList(Ordering.natural().onResultOf(new Function<Node, String>() {
-        @Nullable
-        @Override
-        public String apply(@Nullable Node n) {
-          return n == null ? null : n.getTitle();
-        }
-      }));
+      } else {
+        other.add(n);
+      }
+    }
 
     return SUCCESS;
   }
