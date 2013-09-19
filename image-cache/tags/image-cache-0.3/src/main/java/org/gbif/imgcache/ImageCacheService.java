ca00de481701725b4db8968374965f2f9b7c91ad
federicomh@gmail.com <federicomh@gmail.com@3650c4fd-ea0f-e2d6-5ead-89630c913e81>
2013-09-19 10:46:14 +0000

    [maven-release-plugin]  copy for tag image-cache-0.3

---
+++ image-cache/tags/image-cache-0.3/src/main/java/org/gbif/imgcache/ImageCacheService.java
@@ -0,0 +1,142 @@
+package org.gbif.imgcache;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+import java.net.URLEncoder;
+
+import javax.imageio.ImageIO;
+import javax.inject.Inject;
+
+import com.google.common.base.Preconditions;
+import com.google.common.io.ByteStreams;
+import com.google.common.io.Closer;
+import com.google.inject.name.Named;
+import net.coobird.thumbnailator.Thumbnails;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ImageCacheService {
+
+  private final Logger LOG = LoggerFactory.getLogger(ImageCacheService.class);
+
+  private final File repo;
+  private static final String ENC = "UTF8";
+  private static final String PNG_FMT = "png";
+  private static final String MIME_TYPE = "image/" + PNG_FMT;
+  private static final String DFT_FILENAME = "image";
+
+  @Inject
+  public ImageCacheService(@Named("imgcache.repository") String repository) {
+    repo = new File(repository);
+    LOG.info("Use image repository {}", repo.getAbsolutePath());
+    if (!repo.exists() && !repo.isDirectory()) {
+      throw new IllegalStateException("imgcache.repository needs to be an existing, writable directory: "
+        + repo.getAbsolutePath());
+    }
+  }
+
+  public CachedImage get(URL url, ImageSize size) throws IOException {
+    Preconditions.checkNotNull(url);
+
+    File imgFile = location(url, size);
+    if (!imgFile.exists()) {
+      cacheImage(url);
+    }
+    // TODO: store mime type or deduct from image/file suffix
+    return new CachedImage(url, size, MIME_TYPE, imgFile);
+  }
+
+  private String buildFileName(URL url, ImageSize size) {
+    // try to get some sensible filename - optional
+    String fileName;
+    try {
+      fileName = new File(url.getPath()).getName();
+    } catch (Exception e) {
+      fileName = DFT_FILENAME;
+    }
+
+    if (size != ImageSize.ORIGINAL) {
+      fileName += "-" + size.name().charAt(0) + "." + PNG_FMT;
+    }
+
+    return fileName;
+  }
+
+  private void cacheImage(URL url) throws IOException {
+    // download original
+    LOG.info("Caching image {}", url);
+    copyOriginal(url);
+    // now produce thumbnails from the original
+    produceImage(url, ImageSize.THUMBNAIL, ImageSize.SMALL, ImageSize.MIDSIZE, ImageSize.LARGE);
+  }
+
+  /**
+   * Creates a copy of the file with its original size.
+   */
+  private void copyOriginal(URL url) throws IOException {
+    File origImg = location(url, ImageSize.ORIGINAL);
+
+    OutputStream out = null;
+    InputStream source = null;
+    Closer closer = Closer.create();
+    try {
+      source = closer.register(url.openStream());
+      // create parent folder that is unque for the original image
+      origImg.getParentFile().mkdir();
+      out = closer.register(new FileOutputStream(origImg));
+      ByteStreams.copy(source, out);
+    } finally {
+      closer.close();
+    }
+  }
+
+  /**
+   * Creates a location for the image URL with a suffix for the specified size.
+   */
+  private File location(URL url, ImageSize size) throws IOException {
+    File folder;
+    try {
+      folder = new File(repo, URLEncoder.encode(url.toString(), ENC));
+    } catch (UnsupportedEncodingException e) {
+      LOG.error("Error setting image location", e);
+      throw new IOException("Encoding not supported", e);
+    }
+    return new File(folder, buildFileName(url, size));
+  }
+
+  /**
+   * Produces an image for each size in the sizes paramater.
+   */
+  private void produceImage(URL url, ImageSize... sizes) throws IOException {
+    for (ImageSize size : sizes) {
+      produceImage(url, size);
+    }
+  }
+
+  /**
+   * Produces a single image from the url with the specified size.
+   */
+  private void produceImage(URL url, ImageSize size) throws IOException {
+    File orig = location(url, ImageSize.ORIGINAL);
+    File calc = location(url, size);
+    BufferedImage bufferedImage = ImageIO.read(orig);
+    Thumbnails.Builder<BufferedImage> thumb = Thumbnails.of(bufferedImage)
+      .size(size.width, size.height)
+      .outputFormat(PNG_FMT);
+
+    // make sure thumbnails have the same square size
+    if (ImageSize.THUMBNAIL == size) {
+      thumb.keepAspectRatio(false);
+    }
+
+    // process
+    thumb.toFile(calc);
+    bufferedImage.flush();
+  }
+}
