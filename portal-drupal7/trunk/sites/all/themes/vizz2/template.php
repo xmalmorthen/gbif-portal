5306b4e88dec3137afd71a0d93cc500d12151db5
timrobertson100@gmail.com <timrobertson100@gmail.com@3650c4fd-ea0f-e2d6-5ead-89630c913e81>
2013-09-30 20:29:25 +0000

    Changed case of the title

--- portal-drupal7/trunk/sites/all/themes/vizz2/template.php
+++ portal-drupal7/trunk/sites/all/themes/vizz2/template.php
@@ -918,7 +918,7 @@ echo "            <li><a href='$dataportal_base_url/species'>Explore species</a>
 echo "            <li><a href='$dataportal_base_url/country'>Explore by country</a></li> " ;
 echo "            <li class='divider'></li>" ;
 echo "            <li><a href='$base_url/publishingdata/summary'>Publishing data</a></li> " ;
-echo "            <li><a href='$base_url/usingdata/summary'>Using Data</a></li> " ;
+echo "            <li><a href='$base_url/usingdata/summary'>Using data</a></li> " ;
 echo "            <li><a href='$dataportal_base_url/infrastructure/summary'>Infrastructure</a></li> " ;
 echo '          </ul>
         </div>
