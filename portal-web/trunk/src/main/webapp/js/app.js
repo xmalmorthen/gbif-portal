b993ac8272ce9677ca939d9c0ecb2eef60feec9e
wixner <wixner@3650c4fd-ea0f-e2d6-5ead-89630c913e81>
2013-10-02 12:15:00 +0000

    Allow contact toggling by type too

--- portal-web/trunk/src/main/webapp/js/app.js
+++ portal-web/trunk/src/main/webapp/js/app.js
@@ -186,9 +186,13 @@ $(function() {
   });
 
   // hide and make contact addresses toggable
-  $(".contactName + div").hide();
+  $(".contactName").next().hide();
   $(".contactName").click(function(e){
       $(this).next().slideToggle("fast");
+  });
+  // some contacts don't have a name, allow to also toggle by contact type
+  $(".contactType").click(function(e){
+      $(this).next().next().slideToggle("fast");
   });
   $(".showAllContacts").click(function(e){
       $(".contactName").next().slideToggle("fast");
