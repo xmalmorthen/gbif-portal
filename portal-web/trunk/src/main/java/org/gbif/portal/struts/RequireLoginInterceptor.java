f509f182783d6caa206215bf4320c3f769a3b70e
federicomh@gmail.com <federicomh@gmail.com@3650c4fd-ea0f-e2d6-5ead-89630c913e81>
2013-11-12 13:32:13 +0000

    reverting an accidental commit

--- portal-web/trunk/src/main/java/org/gbif/portal/struts/RequireLoginInterceptor.java
+++ portal-web/trunk/src/main/java/org/gbif/portal/struts/RequireLoginInterceptor.java
@@ -1,14 +1,12 @@
 package org.gbif.portal.struts;
 
 import org.gbif.api.model.common.User;
-import org.gbif.api.vocabulary.UserRole;
+import org.gbif.portal.config.Constants;
 
 import java.util.Map;
 
 import com.opensymphony.xwork2.ActionInvocation;
 import com.opensymphony.xwork2.interceptor.AbstractInterceptor;
-
-import static org.gbif.portal.config.Constants.SESSION_USER;
 
 /**
  * An Interceptor that makes sure a user is currently logged in and returns a notLoggedIn otherwise.
@@ -18,13 +16,11 @@ public class RequireLoginInterceptor extends AbstractInterceptor {
   @Override
   public String intercept(final ActionInvocation invocation) throws Exception {
     final Map session = invocation.getInvocationContext().getSession();
-    User user = new User();
-    user.setUserName("fmendez");
-    user.setKey(1);
-    user.setEmail("fmendez@gbif.org");
-    user.addRole(UserRole.ADMIN);
-    session.put(SESSION_USER, user);
-    return invocation.invoke();
+    final User user = (User) session.get(Constants.SESSION_USER);
+    if (user != null) {
+      return invocation.invoke();
+    }
+    return Constants.RESULT_LOGIN_REQUIRED;
   }
 
 }
