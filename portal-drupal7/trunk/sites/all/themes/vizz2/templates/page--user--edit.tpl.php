2dac447de92e88caee5fc66c492817a763889f40
wixner@googlemail.com <wixner@googlemail.com@3650c4fd-ea0f-e2d6-5ead-89630c913e81>
2013-11-14 10:10:55 +0000

    Update /user/download links and delete unused, deprecated code

--- portal-drupal7/trunk/sites/all/themes/vizz2/templates/page--user--edit.tpl.php
+++ portal-drupal7/trunk/sites/all/themes/vizz2/templates/page--user--edit.tpl.php
@@ -49,7 +49,7 @@
 		<div class="content">
 			<ul>
 				<li class='selected'><a href="<?php print($base_url.'/user/'.$user->uid.'/edit') ?>" title="Summary"><span>Account</span></a></li>
-				<li><a href="<?php print($dataportal_base_url.'/user/downloads') ?>" title="Summary" title="News"><span>Downloads</span></a></li>
+				<li><a href="<?php print($dataportal_base_url.'/user/download') ?>" title="Summary" title="News"><span>Downloads</span></a></li>
 			</ul>
 		</div>
 	</div>
