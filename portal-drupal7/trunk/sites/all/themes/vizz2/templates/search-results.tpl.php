31e1dadb64df916d2374c641cd4ab370d3cee516
gbif.webm <gbif.webm@3650c4fd-ea0f-e2d6-5ead-89630c913e81>
2014-01-27 16:36:26 +0000

    fix the woops for POR-1410

--- portal-drupal7/trunk/sites/all/themes/vizz2/templates/search-results.tpl.php
+++ portal-drupal7/trunk/sites/all/themes/vizz2/templates/search-results.tpl.php
@@ -18,7 +18,6 @@
 			<ul><li>If you are searching for species, datasets or data publishers, see options to the right of this page</li></ul>
 			<p><?php print search_help('search#noresults', drupal_help_arg()); ?></p>
 			<?php endif; ?>
-			<?php krumo($variables) ; ?>
 		</div>
 		<div class="right">
 			<p>This search result only covers the text content of the news and information pages of the GBIF portal.</p>
