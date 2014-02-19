46cdbdde86ff71244146116dd69ad37a2bfc853b
gbif.webm <gbif.webm@3650c4fd-ea0f-e2d6-5ead-89630c913e81>
2014-02-19 15:54:59 +0000

    POR-1792 More progress

--- portal-drupal7/trunk/sites/all/themes/custom/bvng/templates/page.tpl.php
+++ portal-drupal7/trunk/sites/all/themes/custom/bvng/templates/page.tpl.php
@@ -75,55 +75,60 @@
 ?>
 <header id="navbar" role="banner" class="<?php print $navbar_classes; ?>">
   <div class="container">
-    <div class="navbar-header">
-      <?php if ($logo): ?>
-      <a class="logo navbar-btn pull-left" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
-        <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
-      </a>
-      <?php endif; ?>
+	<div class="row">
+		<div class="navbar-header">
+		<?php if ($logo): ?>
+		<a class="logo navbar-btn pull-left" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
+			<img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
+		</a>
+		<?php endif; ?>
 
-      <?php if (!empty($site_name)): ?>
-      <a class="name navbar-brand" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>"><?php print $site_name; ?></a>
-      <?php endif; ?>
+		<?php if (!empty($site_name)): ?>
+		<a class="name navbar-brand" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>"><?php print $site_name; ?></a>
+		<?php endif; ?>
 
-      <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-    </div>
+		<!-- .btn-navbar is used as the toggle for collapsed navbar content -->
+		<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+			<span class="sr-only">Toggle navigation</span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+		</button>
+		</div>
+	</div>
+	<div class="row">
+		<?php if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation'])): ?>
+			<div class="navbar-collapse collapse">
+			<nav role="navigation">
+				<?php if (!empty($primary_nav)): ?>
+				<?php print render($primary_nav); ?>
+				<?php endif; ?>
+				<?php if (!empty($secondary_nav)): ?>
+				<?php print render($secondary_nav); ?>
+				<?php endif; ?>
+				<?php if (!empty($page['navigation'])): ?>
+				<?php print render($page['navigation']); ?>
+				<?php endif; ?>
+			</nav>
+			</div>
+		<?php if (!empty($site_slogan)): ?>
+			<p class="lead"><?php print $site_slogan; ?></p>
+		<?php endif; ?>
 
-    <?php if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation'])): ?>
-      <div class="navbar-collapse collapse">
-        <nav role="navigation">
-          <?php if (!empty($primary_nav)): ?>
-            <?php print render($primary_nav); ?>
-          <?php endif; ?>
-          <?php if (!empty($secondary_nav)): ?>
-            <?php print render($secondary_nav); ?>
-          <?php endif; ?>
-          <?php if (!empty($page['navigation'])): ?>
-            <?php print render($page['navigation']); ?>
-          <?php endif; ?>
-        </nav>
-      </div>
-    <?php if (!empty($site_slogan)): ?>
-      <p class="lead"><?php print $site_slogan; ?></p>
-    <?php endif; ?>
-
-    <?php endif; ?>
-          <?php if (!empty($breadcrumb)): print $breadcrumb; endif;?>
-
+		<?php endif; ?>
+				<?php if (!empty($breadcrumb)): print $breadcrumb; endif;?>
+	</div>
   </div>
 </header>
 
 <div class="infoband-container container">
-      <?php if (!empty($tabs)): ?>
+	<div class="row ">
+	<?php if (!empty($tabs)): ?>
         <?php print render($tabs); ?>
       <?php endif; ?>
-fsadfd
+      <h1>GBIF Newsroom</h1>
+	</div>
+
 </div>
 
 <div class="main-container container well well-lg">
