d23df65ecd99c098d9f0a666fe081433898ca686
bko@gbif.org <bko@gbif.org@3650c4fd-ea0f-e2d6-5ead-89630c913e81>
2014-03-18 08:19:38 +0000

    Set active menu item according to content type.

--- portal-drupal7/trunk/sites/all/themes/custom/bvng/template.php
+++ portal-drupal7/trunk/sites/all/themes/custom/bvng/template.php
@@ -8,14 +8,19 @@
 include_once(drupal_get_path('theme', 'bvng') . '/templates/menu/menu-local-tasks.func.php');
 include_once(drupal_get_path('theme', 'bvng') . '/templates/menu/menu-tree.func.php');
 
+/**
+ * Helper function for showing the title and subtitle of a site section in the
+ * highlighted region.
+ *
+ * The description is retrieved from the description of the menu item.
+ */
 function bvng_get_title_data() {
 
-  /* The old way
-	$trail = menu_get_active_trail() ;
-	$taxon = taxonomy_get_term_by_name($trail[2]['title'], 'taxanavigation');
-	reset($taxon);
-	return current($taxon);
-	*/
+  // The old way
+	// $trail = menu_get_active_trail() ;
+	// $taxon = taxonomy_get_term_by_name($trail[2]['title'], 'taxanavigation');
+	// reset($taxon);
+	// return current($taxon);
 	
 	// This way disassociates the taxanavigation voc, is more reasonable, but a bit heavy.
 	// Only 'GBIF Newsroom' has a shorter name in the nav.
@@ -26,7 +31,9 @@ function bvng_get_title_data() {
    'name' => ($parent['link_title'] == 'GBIF News') ? 'GBIF Newsroom' : $parent['link_title'],
 	 'description' => $parent['options']['attributes']['title'],
 	);
-		
 	return $title;
 }
 
+function bvng_preprocess_page(&$variables) {
+  $variables['page']['highlighted_title'] = bvng_get_title_data();
+}
