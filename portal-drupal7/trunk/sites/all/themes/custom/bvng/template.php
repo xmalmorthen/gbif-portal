cf7fc8727abb1730cd4b0a4a22677cd5016b96bd
bko@gbif.org <bko@gbif.org@3650c4fd-ea0f-e2d6-5ead-89630c913e81>
2014-03-28 13:00:43 +0000

    POR-1898 done the news item page.

--- portal-drupal7/trunk/sites/all/themes/custom/bvng/template.php
+++ portal-drupal7/trunk/sites/all/themes/custom/bvng/template.php
@@ -26,7 +26,7 @@ function bvng_get_title_data() {
 	// $taxon = taxonomy_get_term_by_name($trail[2]['title'], 'taxanavigation');
 	// reset($taxon);
 	// return current($taxon);
-	
+
 	// This way disassociates the taxanavigation voc, is more reasonable, but a bit heavy.
 	// Only 'GBIF Newsroom' has a shorter name in the nav.
 	$active_menu_item = menu_link_get_preferred(current_path(), 'gbif-menu');
@@ -52,36 +52,45 @@ function bvng_preprocess_page(&$variables) {
         $system_path = drupal_get_normal_path('newsroom/news'); // taxonomy/term/566
         menu_tree_set_path('gbif-menu', $system_path);
         menu_set_active_item($system_path);
+        $variables['node']->type_title = t('GBIF News');
         break;
       case 'usesofdata':
         $system_path = drupal_get_normal_path('newsroom/uses'); // taxonomy/term/566
         menu_tree_set_path('gbif-menu', $system_path);
         menu_set_active_item($system_path);
+        $variables['node']->type_title = t('Featured Data Use');
         break;
     }
   }
-  
+
   $variables['page']['highlighted_title'] = bvng_get_title_data();
-  
+
   // @todo For testing purpose. To be deleted later.
   // drupal_set_message(t('An error messaged is generated for developing the message box.'), 'warning');
 }
 
 /**
- * Implements hook_preprocess_node(). 
+ * Implements hook_preprocess_node().
  */
 function bvng_preprocess_node(&$variables) {
+  /* Get sidebar content
+   */
+  $sidebar = bvng_get_sidebar_content($variables['nid'], $variables['vid']);
+  $variables['sidebar'] = $sidebar;
+
+  /* Processing menu_local_tasks()
+   */
   global $user;
 
   // Bring the local tasks tab into node template.
   $variables['tabs'] = menu_local_tabs();
-  
+
   /* Determine what local task to show according to the role.
    * @todo To implement this using user_permission.
    */
   foreach ($variables['tabs']['#primary'] as $key => $item) {
     // We don't need the view tab because we're already viewing it.
-    
+
     switch ($item['#link']['title']) {
       case 'View':
         unset($variables['tabs']['#primary'][$key]);
@@ -97,4 +106,76 @@ function bvng_preprocess_node(&$variables) {
 }
 
 function bvng_preprocess_search_block_form(&$variables) {
+}
+
+function bvng_get_sidebar_content($nid, $vid) {
+  $node = node_load($nid, $vid);
+
+  if ($node && $node->type === 'newsarticle') {
+
+    // PUBLICATION DATE
+    $publication_date = array(
+      'title' => t('Publication date'),
+      'type' => 'ul',
+      'items' => array(
+        'data' => format_date($node->created, 'custom', 'F jS, Y '),
+      ),
+      'attributes' => array(
+        'id' => 'publication-date',
+        'class' => 'item-date',
+      ),
+    );
+
+    // LAST UPDATED
+    $last_updated = array(
+      'title' => t('Last updated'),
+      'type' => 'ul',
+      'items' => array(
+        'data' => format_date($node->changed, 'custom', 'F jS, Y '),
+      ),
+      'attributes' => array(
+        'id' => 'last-updated',
+        'class' => 'item-date',
+      ),
+    );
+
+    // TAGS
+    $terms = array();
+    // List where to get terms to populate as tags.
+    $term_sources = array(
+      'field_capacity',
+      'field_country',
+      'field_informatics',
+      'field_organizations',
+      'field_regions',
+    );
+
+    // Get all the terms.
+    foreach ($term_sources as $term_source) {
+      $items = field_get_items('node', $node, $term_source);
+      foreach ($items as $item) {
+        $terms[] = $item['tid'];
+      }
+    }
+    // Create term links.
+    $term_links = array(
+      'title' => t('Tags'),
+      'type' => 'ul',
+      'attributes' => array(
+        'id' => 'tags',
+        'class' => 'tags'
+      ),
+    );
+    foreach ($terms as $tid) {
+      $term = taxonomy_term_load($tid);
+      $uri = taxonomy_term_uri($term);
+      $term_links['items'][]['data'] = l($term->name, $uri['path']);
+    }
+  }
+
+  $markup = theme_item_list($publication_date);
+  $markup .= theme_item_list($last_updated);
+  $markup .= theme_item_list($term_links);
+
+  return $markup;
 }
