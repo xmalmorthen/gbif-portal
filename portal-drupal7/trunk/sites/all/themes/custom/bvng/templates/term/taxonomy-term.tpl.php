86b62b951d1f6b7b6288c8c11f9d3f88fe6d3f27
bko@gbif.org <bko@gbif.org@3650c4fd-ea0f-e2d6-5ead-89630c913e81>
2014-04-01 07:31:19 +0000

    [draft] Latest news items.

--- portal-drupal7/trunk/sites/all/themes/custom/bvng/templates/term/taxonomy-term.tpl.php
+++ portal-drupal7/trunk/sites/all/themes/custom/bvng/templates/term/taxonomy-term.tpl.php
@@ -41,15 +41,17 @@
  * @ingroup themeable
  */
 ?>
-<div id="taxonomy-term-<?php print $term->tid; ?>" class="<?php print $classes; ?>">
+<div class="row">
+  <div id="taxonomy-term-<?php print $term->tid; ?>" class="<?php print $classes; ?>">
+    <?php if (!$page): ?>
+      <h2><a href="<?php print $term_url; ?>"><?php print $term_name; ?></a></h2>
+    <?php endif; ?>
 
-  <?php if (!$page): ?>
-    <h2><a href="<?php print $term_url; ?>"><?php print $term_name; ?></a></h2>
-  <?php endif; ?>
-
-  <div class="content">
-    term page template in effect
-    <?php print render($content); ?>
+      <header class="content-header col-md-8">
+        <h2>Latest news items</h2>
+      </header>
+      <div class="sidebar-filter col-md-4">
+        Filter by region.
+      </div>
   </div>
-
 </div>
