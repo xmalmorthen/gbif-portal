ae88916171fe473290b526621ec6ebbbb7f5f53e
bko@gbif.org <bko@gbif.org@3650c4fd-ea0f-e2d6-5ead-89630c913e81>
2014-04-15 21:11:41 +0000

    Trial login template.

--- portal-drupal7/trunk/sites/all/themes/custom/bvng/templates/system/user-login.tpl.php
+++ portal-drupal7/trunk/sites/all/themes/custom/bvng/templates/system/user-login.tpl.php
@@ -4,10 +4,20 @@
  * Default theme implementation to user login page.
  */
 ?>
-<?php
-  print drupal_render($form['name']);
-  print drupal_render($form['pass']);
-  print drupal_render($form['form_build_id']);
-  print drupal_render($form['form_id']);
-  print drupal_render($form['actions']);
-?>
+<div class="row user-login-row">
+  <div class="subscribe col-md-8">
+    <?php
+      print drupal_render($form['name']);
+      print drupal_render($form['pass']);
+      print drupal_render($form['form_build_id']);
+      print drupal_render($form['form_id']);
+    ?>
+  </div>
+</div>
+<div class="row user-login-action">
+  <div class="col-md-8">
+    <?php print drupal_render($form['actions']); ?>
+    <p>Forgot your password?</p>
+    <p>Do you need to sign up? <a href="#">Create your account</a></p>
+  </div>
+</div>
