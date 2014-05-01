a18c7fec3f90c8a39e9f5356c7d3a8b3d9c2894e
gbif.webm <gbif.webm@3650c4fd-ea0f-e2d6-5ead-89630c913e81>
2014-05-01 18:23:54 +0000

    import for resources and events accounts for empty string fields, no need for separate templates

--- portal-drupal7/trunk/sites/all/themes/custom/bvng/templates/field/field--body--event_ims.tpl.php
+++ portal-drupal7/trunk/sites/all/themes/custom/bvng/templates/field/field--body--event_ims.tpl.php
@@ -51,7 +51,7 @@
 		<?php endif; ?>
 		<div class="field-items"<?php print $content_attributes; ?>>
 			<?php foreach ($items as $delta => $item): ?>
-				<div class="field-item <?php print $delta % 2 ? 'odd' : 'even'; ?>"<?php print $item_attributes[$delta]; ?>><?php print render($item); var_dump($item) ;?></div>
+				<div class="field-item <?php print $delta % 2 ? 'odd' : 'even'; ?>"<?php print $item_attributes[$delta]; ?>><?php print render($item); ?></div>
 			<?php endforeach; ?>
 		</div>
 	</div>
